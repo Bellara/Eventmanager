114b0bfc21b620d10e63c235259b92fb25b3023f
seppno95 <seppno95@googlemail.com>
2014-11-06 17:24:06 +0100

    Listview mit Testdaten hinzugefügt.

--- src/whs/bocholt/Eventmanager/MyActivity.java
+++ src/whs/bocholt/Eventmanager/MyActivity.java
@@ -1,9 +1,11 @@
 package whs.bocholt.Eventmanager;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MyActivity extends Activity {
+public class MyActivity extends ListActivity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +13,9 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        String[] events ={"Test1", "Test2", "Party bei Tom", "otto hat Spass"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, events);
+        getListView().setAdapter(adapter);
     }
 }
